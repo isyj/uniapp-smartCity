248527bbc78b064c610ea421cfa261582c249d73
imxiaochen0v0 <146860695+imxiaochen0v0@users.noreply.github.com>
2023-11-15 05:00:09 +0800

    解决api里图片需要拼接服务器地址问题

--- uniapp_project/pages/subPages/search/newsDetails.vue
+++ uniapp_project/pages/subPages/search/newsDetails.vue
@@ -4,7 +4,8 @@
 			<uni-nav-bar rightWidth="10rpx" leftWidth="50rpx" left-icon="back" :title="this.list.title" statusBar
 				@clickLeft="backSearch()"></uni-nav-bar>
 		</view>
-		<view class="content" v-html="this.list.content"></view>
+		<view class="content" v-html="this.list.content.replace(/\/prod-api/g, 'http://124.93.196.45:10001/prod-api')">
+		</view>
 	</view>
 </template>
 
@@ -18,6 +19,7 @@
 		onLoad(e) {
 			this.$request('/prod-api/press/press/' + e.id, '', 'GET').then(res => {
 				this.list = res.data.data
+				console.log(this.list.content.replace(/\/prod-api/g, 'http://124.93.196.45:10001/prod-api'));
 			})
 		},
 		methods: {
@@ -38,6 +40,10 @@
 			box-sizing: border-box;
 			padding: 20rpx;
 			margin: 20rpx;
+
+			/deep/img {
+				width: 100%;
+			}
 		}
 	}
 </style>
