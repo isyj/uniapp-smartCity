8e2cd5e8b03a2fdff14297155802557bb0439cc3
imxiaochen0v0 <146860695+imxiaochen0v0@users.noreply.github.com>
2023-12-04 01:18:31 +0800

    订单列表

--- uniapp_project/pages/user/allOrder.vue
+++ uniapp_project/pages/user/allOrder.vue
@@ -1,11 +1,21 @@
 <template>
 	<view>
+
+		<view class="orders">
+			<view class="content" v-for="(item,index) in orderClassify" :key="index" @click="click(index)">
+				<view :class="{btn:count == index}">
+					{{item}}
+				</view>
+			</view>
+		</view>
+
 		<view>
-			<uni-card :title="item.name" :sub-title="item.orderNo" :extra="item.orderTypeName"
+			<uni-card :title=" item.name" :sub-title="item.orderNo" :extra="item.orderTypeName"
 				v-for="(item,index) in list" :key="index">
 				<text>{{item.orderStatus}}</text>
 			</uni-card>
 		</view>
+
 	</view>
 </template>
 
@@ -16,7 +26,9 @@
 	export default {
 		data() {
 			return {
+				count: 0,
 				list: [],
+				orderClassify: ['全部订单', '订单分类']
 			};
 		},
 		async onLoad() {
@@ -25,11 +37,35 @@
 			})
 		},
 		methods: {
-
+			click(index) {
+				this.count = index
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	.orders {
+		display: flex;
+		margin: 25rpx 0;
 
+		.content {
+			width: 50%;
+			text-align: center;
+		}
+
+		.btn {
+			color: pink;
+		}
+
+		.btn::after {
+			content: '';
+			width: 125rpx;
+			height: 5rpx;
+			background-color: pink;
+			position: absolute;
+			margin-top: 50rpx;
+			margin-left: -126rpx;
+		}
+	}
 </style>
